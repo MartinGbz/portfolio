2595eb2923223d69b8101be564a827500d4ce64d
MartinGbz <martin.grabarz@outlook.fr>
2024-01-06 23:53:20 +0100

    feat: add "Built with ❤️"

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -8,12 +8,18 @@ interface FooterProps {
 
 export function Footer({ className }: FooterProps) {
   return (
-    <Image
-      className="w-screen"
-      src={"/wave.png"}
-      alt="github"
-      height={208}
-      width={1000}
-    />
+    <div className="relative">
+      <Image
+        className="w-screen"
+        src={"/wave.png"}
+        alt="github"
+        height={208}
+        width={1000}
+      />
+      <p className="absolute bottom-4 left-1/2 text-center -translate-x-1/2">
+        {" "}
+        Built with ❤️ by MartinGbz{" "}
+      </p>
+    </div>
   );
 }
