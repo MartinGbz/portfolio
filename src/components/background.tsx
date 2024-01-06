80d69794043fffc205f8df651b8f10214902a666
MartinGbz <martin.grabarz@outlook.fr>
2024-01-06 23:34:30 +0100

    fix: background

--- src/components/background.tsx
+++ src/components/background.tsx
@@ -19,7 +19,7 @@ export function Background() {
     // (because th whole page is creating before the background is rendered because of hydratation issues)
     browsersNotSupported.includes(browserName) ? (
       <div className="absolute w-screen h-screen top-0 left-0 -z-10 blur-2xl animate-fade-in overflow-x-clip">
-        <div className="absolute w-screen max-w-[2500px] h-screen -top-1/4 lg:-top-1/2 -left-1/2">
+        <div className="absolute w-screen max-w-[2500px] h-screen -top-1/4 xl:-top-1/2 -left-1/2">
           <object type="image/svg+xml" data="blobs/blob1.svg" />
         </div>
       </div>
